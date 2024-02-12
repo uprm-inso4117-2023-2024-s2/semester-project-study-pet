e7e0f0451f50781ae06f892341f265fa71241b64
William A. Martínez Martínez <william.martinez10@upr.edu>
2024-02-12 19:37:46 -0400

    created flashcard creator component

---
+++ components/FlashCardCreator.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+export default function FlashCardCreator(props) {
+  const [studySet, setStudySet] = useState('');
+  const [question, setQuestion] = useState('');
+  const [answer, setAnswer] = useState('');
+
+  function handleSubmit(e) {
+    setStudySet('');
+    setQuestion('');
+    setAnswer('');
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.formContainer}>
+        <Text style={styles.legend}>New Flashcards</Text>
+        <TextInput
+            style={[styles.input, styles.textInput]}
+            placeholder="Study Set"
+            placeholderTextColor="gray"
+            value={studySet}
+            onChangeText={setStudySet}
+        />
+        <TextInput
+            style={[styles.input, styles.textInput]}
+            placeholder="Question"
+            placeholderTextColor="gray"
+            value={question}
+            onChangeText={setQuestion}
+            multiline={true}
+        />
+        <TextInput
+            style={[styles.input, styles.textInput]}
+            placeholder="Answer"
+            placeholderTextColor="gray"
+            value={answer}
+            onChangeText={setAnswer}
+            multiline={true}
+        />
+        <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+            <Text>Create</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+  }
+
+const styles = StyleSheet.create({
+  formContainer: {
+      alignSelf: 'center',
+      backgroundColor: '#f5f5dc',
+      padding: 30,
+      width: 300,
+      borderRadius: 20,
+      shadowColor: '#000',
+      shadowOffset: { width: 5, height: 5 },
+      shadowOpacity:  0.4,
+      shadowRadius: 3,
+      marginBottom: 15,
+  },
+  legend: {
+      textAlign: 'center',
+      fontSize: 22,
+      borderRadius: 20,
+      marginBottom: 10,
+      paddingBottom: 15,
+  },
+  input: {
+      borderColor: '#000',
+      borderRadius: 5,
+      padding: 3,
+      fontSize: 16,
+      marginBottom: 15,
+      borderWidth: 1,
+  },
+  button: {
+      display: 'inline-block',
+      alignSelf: 'center',
+      backgroundColor: '#ffa07a',
+      paddingVertical: 10,
+      paddingHorizontal: 50,
+      fontSize: 20,
+      borderRadius: 10,
+  },
+});
