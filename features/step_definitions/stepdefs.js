b1075b9b2e521b4ba6f094f32e0db47c84d72985
Diego A. Velez Torres <diego.velez6@upr.edu>
2024-02-21 20:37:41 -0400

    Added flashcard BDD tests and their respective logic with TODOs

---
+++ features/step_definitions/stepdefs.js
@@ -0,0 +1,46 @@
+const {Given, When, Then} = require('@cucumber/cucumber');
+
+Given("User at Overview Screen", () => {
+    // TODO: Navigate app to overview screen
+    return "pending";
+});
+
+When("User Clicks The First FlashCard", () => {
+    // TODO: Get first flashcard in screen and click it
+    return "pending";
+});
+
+When("User Clicks The Add FlashCard Button", () => {
+    // TODO: Get the add flashcard button and click it
+    return "pending";
+});
+
+When("User Fills FlashCard Form", () => {
+    // TODO: Fill flashcard form in the add flashcard screen
+    return "pending";
+});
+
+Given("User Clicks The Submit Button", () => {
+    // TODO: Click the submit button in the add flashcard screen
+    return "pending";
+});
+
+Given("Verify New FlashCard Appears", () => {
+    // TODO: Verify that the new flashcard added appears in the overview screen
+    return "pending";
+});
+
+Then("The First FlashCard Flips", () => {
+    // TODO: Verify the first flashcard is flipped
+    return "pending";
+});
+
+Then("Navigate to Add FlashCard Screen", () => {
+    // TODO: Navigate to add flashcard screen
+    return "pending";
+});
+
+Then("Navigate to Overview Screen", () => {
+    // TODO: Navigate to overview screen
+    return "pending";
+});
