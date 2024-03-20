6dafed51d8d0feea253afafe4427d5f94262caa6
joel-alvarado <joel.alvarado2@upr.edu>
2024-03-20 19:33:05 -0400

    Refactor Shop component to include new features

--- pages/Shop.js
+++ pages/Shop.js
@@ -1,49 +1,55 @@
 import { Ionicons } from "@expo/vector-icons";
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity, FlatList, Image } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  Image,
+  Pressable,
+} from "react-native";
 import { StyleSheet } from "react-native";
 import { useFonts } from "expo-font";
 
-const Shop = ({ navigation }) => {
+const Shop = ({ studyPoints, setStudyPoints }) => {
   const [dummy] = useFonts({
     "Jua-Regular": require("../assets/fonts/Jua-Regular.ttf"),
   });
-  const [studyPoints, setStudyPoints] = useState(100);
   const [category, setCategory] = useState("All");
-  const categories = ["All", "Category 1", "Category 2"];
+  const categories = ["All", "Food", "Toys", "Clothes", "Other"];
   const [items, setItems] = useState([
     {
       id: 1,
-      name: "Item 1",
-      category: "Category 1",
+      name: "Orange Juice",
+      category: "Food",
       cost: 10,
       image: "https://via.placeholder.com/50",
     },
     {
       id: 2,
-      name: "Item 2",
-      category: "Category 1",
+      name: "Apple Juice",
+      category: "Food",
       cost: 20,
       image: "https://via.placeholder.com/50",
     },
     {
       id: 3,
-      name: "Item 3",
-      category: "Category 2",
+      name: "Banana Juice",
+      category: "Food",
       cost: 20,
       image: "https://via.placeholder.com/50",
     },
     {
       id: 4,
-      name: "Item 4",
-      category: "Category 2",
-      cost: 30,
+      name: "Ball",
+      category: "Toys",
+      cost: 50,
       image: "https://via.placeholder.com/50",
     },
     {
       id: 5,
-      name: "Item 5",
-      category: "Category 2",
+      name: "Los Diablos Ski Mask",
+      category: "Clothes",
       cost: 40,
       image: "https://via.placeholder.com/50",
     },
@@ -85,42 +91,82 @@ const Shop = ({ navigation }) => {
 
   const renderCategory = ({ item }) => (
     <View style={styles.category}>
-      <TouchableOpacity onPress={() => setCategory(item)}>
+      <TouchableOpacity
+        onPress={() => setCategory(item)}
+        styles={styles.category}
+      >
         <Text style={styles.categoryText}>{item}</Text>
       </TouchableOpacity>
     </View>
   );
 
   return (
-    <View style={{ justifyContent: "center", alignItems: "left" }}>
-      <FlatList
-        style={{
-          alignSelf: "center",
-          margin: 5,
-          padding: 5,
-          flexDirection: "row",
-        }}
-        data={categories}
-        renderItem={renderCategory}
-        horizontal={true}
-      />
+    <View style={styles.shopContainer}>
+      <View style={{ flexDirection: "row" }}>
+        <View
+          style={{
+            width: 60,
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+            marginLeft: 10,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 26,
+              fontFamily: "Jua-Regular",
+              alignSelf: "center",
+            }}
+          >
+            {studyPoints}
+          </Text>
+          <Ionicons
+            name="logo-bitcoin"
+            size={24}
+            color="black"
+            style={{ alignSelf: "center" }}
+          />
+        </View>
+        <FlatList
+          style={{
+            alignSelf: "center",
+            margin: 5,
+            padding: 5,
+            flexDirection: "row",
+          }}
+          data={categories}
+          renderItem={renderCategory}
+          horizontal={true}
+        />
+      </View>
       <FlatList
         data={items.filter(
           (item) => item.category === category || category === "All"
         )}
         renderItem={renderItem}
       />
+      <Pressable style={{ padding: 10 }} onPress={() => setStudyPoints(999)}>
+        <Text style={{ fontSize: 26, fontFamily: "Jua-Regular" }}>
+          Reset points
+        </Text>
+      </Pressable>
     </View>
   );
 };
 
 // Stylesheet for item, shows item, name, cost and category with picture of item
 const styles = StyleSheet.create({
+  shopContainer: {
+    flex: 1,
+    backgroundColor: "#89CFF0",
+    paddingBottom: 25,
+  },
   item: {
     padding: 5,
     flexDirection: "row",
-    backgroundColor: "#98FB98",
+    backgroundColor: "#ffffff",
     margin: 5,
+    borderRadius: 10,
   },
   image: {
     width: 50,
@@ -144,6 +190,7 @@ const styles = StyleSheet.create({
     padding: 5,
     backgroundColor: "#98FB98",
     margin: 5,
+    borderRadius: 10,
   },
   categoryText: {
     fontSize: 26,
