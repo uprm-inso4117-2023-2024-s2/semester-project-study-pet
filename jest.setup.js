03e97e51fd4398152b17aaf98e313037bf4b2b38
FrancesC8 <frances.caban2@upr.edu>
2024-03-21 17:16:18 -0700

    Consolidated jest setup file into one.

--- jest.setup.js
+++ jest.setup.js
@@ -8,5 +8,10 @@ module.exports = {
   preset: "jest-expo",
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
-  ]
+  ],
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[tj]s?(x)"
+  ],
+  clearMocks: true,
 };
