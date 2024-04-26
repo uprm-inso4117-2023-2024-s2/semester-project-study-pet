00e1cea996c4892e01ad73980f48f6242c5c6157
Diego A. Velez Torres <diego.velez6@upr.edu>
2024-04-26 09:19:21 -0400

    Add BDD tests for the Shop page

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -2,6 +2,7 @@ const {Given, When, Then, BeforeAll, AfterAll} = require('@cucumber/cucumber');
 const {Browser, Builder, By} = require("selenium-webdriver");
 const {expect} = require("expect");
 const questionsData = require("../../assets/data/questions.json");
+const {NoSuchElementError} = require("selenium-webdriver/lib/error");
 
 let driver;
 
@@ -13,6 +14,7 @@ AfterAll(async () => {
     await driver.quit();
 });
 
+////// Game Tests
 Given("User at Home Page", async () => {
     await driver.get("http://localhost:8081");
 
@@ -61,7 +63,6 @@ Then("User At Game Page", async () => {
 });
 
 Then("User At The First Question", async () => {
-    await driver.manage().setTimeouts({implicit: 1000});
     const questionText = await driver.findElement(By.xpath(GamePageMiniGame.questionText));
     expect(await questionText.getText()).toBe(questionsData[0].question);
 });
@@ -71,8 +72,121 @@ Then("Scoring Should Be {int} of {int}", async (score, totalQuestions) => {
     expect(await resultText.getText()).toBe(`You got ${score} out of ${totalQuestions} correct!`);
 });
 
+////// Shop Tests
+When("User Clicks The Navigate to Shop Page Button", async () => {
+    const navigateToShopPage = await driver.findElement(By.xpath(HomePage.navigateToShopPage));
+    navigateToShopPage.click();
+});
+
+Then("User At Shop Page", async () => {
+    expect(await driver.getTitle()).toBe("Shop");
+});
+
+When("User Filters by {string}", async (filter) => {
+    let filterButtonXpath;
+    switch (filter) {
+        case "All":
+            filterButtonXpath = ShopPage.allFilter;
+            break;
+        case "Food":
+            filterButtonXpath = ShopPage.foodFilter;
+            break;
+        case "Toys":
+            filterButtonXpath = ShopPage.toysFilter;
+            break;
+        case "Clothes":
+            filterButtonXpath = ShopPage.clothesFilter;
+            break;
+        case "Other":
+            filterButtonXpath = ShopPage.otherFilter;
+            break;
+    }
+
+    const filterButton = await driver.findElement(By.xpath(filterButtonXpath));
+    filterButton.click();
+});
+
+Then("{string} Items Show", async (filter) => {
+    let itemsThatShouldShow = [];
+    switch (filter) {
+        case "All":
+            itemsThatShouldShow = ShopPage.allItems;
+            break;
+        case "Food":
+            itemsThatShouldShow = [ShopPage.itemOne, ShopPage.itemTwo, ShopPage.itemThree];
+            break;
+        case "Toys":
+        case "Clothes":
+            itemsThatShouldShow = [ShopPage.itemOne];
+            break;
+    }
+
+    for (const itemXpath of itemsThatShouldShow) {
+        await driver.manage().setTimeouts({implicit: 1000});
+
+        const item = await driver.findElement(By.xpath(itemXpath));
+        item.click();
+    }
+
+    const itemsThatShouldNotShow = ShopPage.allItems.filter(itemXpath => !itemsThatShouldShow.includes(itemXpath));
+    for (const itemXpath of itemsThatShouldNotShow) {
+        await driver.manage().setTimeouts({implicit: 1000});
+
+        try {
+            await driver.findElement(By.xpath(itemXpath));
+            throw new Error("Item should not exist!");
+        } catch (e) {
+            if (!(e instanceof NoSuchElementError)) {
+                throw new Error("Item should not exist!");
+            }
+        }
+    }
+});
+
+When("User Buys {string}", async (itemName) => {
+    if (itemName === "All") {
+        for (const itemXpath of ShopPage.allItems) {
+            await driver.manage().setTimeouts({implicit: 1000});
+            const item = await driver.findElement(By.xpath(itemXpath));
+            item.click();
+        }
+    } else {
+        let itemXpath;
+        switch (itemName) {
+            case "Orange Juice":
+                itemXpath = ShopPage.itemOne;
+                break;
+            case "Apple Juice":
+                itemXpath = ShopPage.itemTwo;
+                break;
+            case "Banana Juice":
+                itemXpath = ShopPage.itemThree;
+                break;
+            case "Ball":
+                itemXpath = ShopPage.itemFour;
+                break;
+            case "Los Diablos Ski Mask":
+                itemXpath = ShopPage.itemFive;
+                break;
+        }
+        const item = await driver.findElement(By.xpath(itemXpath));
+        item.click();
+    }
+});
+
+When("User Resets Money", async () => {
+    const resetMoney = await driver.findElement(By.xpath(ShopPage.reset));
+    resetMoney.click();
+});
+
+Then("Money Left is {string}", async (balance) => {
+    const money = await driver.findElement(By.xpath(ShopPage.money));
+    expect(await money.getText()).toBe(balance);
+});
+
 class HomePage {
     static navigateToGamePage = "/html/body/div[1]/div/div/div/div[1]/div/div[2]/div[2]/div/div/div/div[1]/div/div/div[3]/div[3]";
+    static navigateToShopPage = "/html/body/div[1]/div/div/div[2]/div[1]/div/div[2]/div[2]/div/div/div/div[1]/div/div/div[3]/div[5]";
 }
 
 class GamePageMiniGame {
@@ -138,4 +252,23 @@ class GamePageMiniGame {
             correctAnswer.click()
         }
     }
+}
+
+class ShopPage {
+    static money = "/html/body/div[1]/div/div/div[2]/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[1]/div[1]/div[1]";
+
+    static allFilter = "/html/body/div[1]/div/div/div[2]/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[1]/div[2]/div/div[1]/div/div";
+    static foodFilter = "/html/body/div[1]/div/div/div[2]/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[1]/div[2]/div/div[2]/div/div";
+    static toysFilter = "/html/body/div[1]/div/div/div[2]/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[1]/div[2]/div/div[3]/div/div";
+    static clothesFilter = "/html/body/div[1]/div/div/div[2]/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[1]/div[2]/div/div[4]/div/div";
+    static otherFilter = "/html/body/div[1]/div/div/div[2]/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[1]/div[2]/div/div[5]/div/div";
+
+    static itemOne = "/html/body/div[1]/div/div/div[2]/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[2]/div/div[1]/div";
+    static itemTwo = "/html/body/div[1]/div/div/div[2]/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[2]/div/div[2]/div";
+    static itemThree = "/html/body/div[1]/div/div/div[2]/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[2]/div/div[3]/div";
+    static itemFour = "/html/body/div[1]/div/div/div[2]/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[2]/div/div[4]/div";
+    static itemFive = "/html/body/div[1]/div/div/div[2]/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[2]/div/div[5]/div";
+    static allItems = [ShopPage.itemOne, ShopPage.itemTwo, ShopPage.itemThree, ShopPage.itemFour, ShopPage.itemFive];
+
+    static reset = "/html/body/div[1]/div/div/div[2]/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[3]";
 }
