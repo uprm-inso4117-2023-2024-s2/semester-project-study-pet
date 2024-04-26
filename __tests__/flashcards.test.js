92aa7f7059ce262c2f02a972b71fe7c1793fde43
Diego A. Velez Torres <diego.velez6@upr.edu>
2024-04-26 14:29:01 -0400

    Extract flashcards tests from studypet file/tests into its own file

---
+++ __tests__/flashcards.test.js
@@ -0,0 +1,116 @@
+import {Browser, Builder, By} from "selenium-webdriver"
+import {NoSuchElementError} from "selenium-webdriver/lib/error";
+
+describe('the flashcards page', () => {
+    let driver;
+
+    beforeEach(async () => {
+        driver = await new Builder().forBrowser(Browser.FIREFOX).build();
+    });
+
+    it("navigates to the flashcards page", async () => {
+        await driver.get("http://localhost:8081");
+
+        expect(await driver.getTitle()).toBe("Home");
+
+        const navigateToFlashcard = await driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[1]/div/div[2]/div[2]/div/div/div/div[1]/div/div/div[1]/div[3]"));
+        navigateToFlashcard.click();
+
+        await driver.manage().setTimeouts({implicit: 1000});
+
+        expect(await driver.getTitle()).toBe("Flashcards");
+    });
+
+    it("creates a flashcard", async () => {
+        await driver.get("http://localhost:8081");
+
+        const navigateToFlashcard = await driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[1]/div/div[2]/div[2]/div/div/div/div[1]/div/div/div[1]/div[3]"));
+        navigateToFlashcard.click();
+
+        await driver.manage().setTimeouts({implicit: 1000});
+
+        const addFlashCard = await driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[3]"));
+        addFlashCard.click();
+
+        await driver.manage().setTimeouts({implicit: 1000});
+
+        const studySet = await driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[2]/div/div[1]/div/input"));
+        studySet.sendKeys("INSO");
+
+        const question = await driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[2]/div/div[1]/div/textarea[1]"));
+        question.sendKeys("What is OOP?");
+
+        const answer = await driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[2]/div/div[1]/div/textarea[2]"));
+        answer.sendKeys("Object Oriented Programming");
+
+        const create = await driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[2]/div/div[1]/div/div[2]"));
+        create.click();
+
+        await driver.manage().setTimeouts({implicit: 1000});
+
+        const flashcardTextQuestionSide = await driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[2]/div/div/div/div[2]/div/div/div/div/div/div/div"));
+        expect(await flashcardTextQuestionSide.getText()).toBe("What is OOP?");
+
+        flashcardTextQuestionSide.click();
+
+        await driver.manage().setTimeouts({implicit: 1000});
+
+        const flashcardTextAnswerSide = await driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[2]/div/div/div/div[2]/div/div/div/div/div/div/div"));
+        expect(await flashcardTextAnswerSide.getText()).toBe("Object Oriented Programming");
+    });
+
+    it("removes a flashcard", async () => {
+        await driver.get("http://localhost:8081");
+
+        const navigateToFlashcard = await driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[1]/div/div[2]/div[2]/div/div/div/div[1]/div/div/div[1]/div[3]"));
+        navigateToFlashcard.click();
+
+        await driver.manage().setTimeouts({implicit: 1000});
+
+        const addFlashCard = await driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[3]"));
+        addFlashCard.click();
+
+        await driver.manage().setTimeouts({implicit: 1000});
+
+        let studySet = await driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[2]/div/div[1]/div/input"));
+        studySet.sendKeys("INSO");
+
+        let question = await driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[2]/div/div[1]/div/textarea[1]"));
+        question.sendKeys("What is OOP?");
+
+        const answer = await driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[2]/div/div[1]/div/textarea[2]"));
+        answer.sendKeys("Object Oriented Programming");
+
+        const create = await driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[2]/div/div[1]/div/div[2]"));
+        create.click();
+
+        const removeFlashcard = await driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[4]"));
+        removeFlashcard.click();
+
+        await driver.manage().setTimeouts({implicit: 1000});
+
+        studySet = await driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[2]/div/div[1]/div/input"));
+        studySet.sendKeys("INSO");
+
+        question = await driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[2]/div/div[1]/div/textarea"));
+        question.sendKeys("What is OOP?");
+
+        const remove = await driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[2]/div/div[1]/div/div[2]"));
+        remove.click();
+
+        await driver.manage().setTimeouts({implicit: 1000});
+
+        try {
+            await driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]/div/div[2]/div/div/div/div[2]/div/div/div/div/div/div/div"))
+            throw new Error("Flashcard should not exist!");
+        } catch (e) {
+            if (!(e instanceof NoSuchElementError)) {
+                throw new Error("Flashcard should not exist!");
+            }
+        }
+    });
+
+    afterEach(async () => {
+        await driver.quit();
+    });
+});
