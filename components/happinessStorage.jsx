b70c579ad33e516b6ec12b88e4c39abccde6e8e2
CaribbeanCool <alejandro.perez21@upr.edu>
2024-04-27 22:22:47 -0400

    print statements

--- components/happinessStorage.jsx
+++ components/happinessStorage.jsx
@@ -6,6 +6,7 @@ export const saveHappiness = async (happiness) => {
     try {
         // Limit happiness to 100
         const cappedHappiness = Math.min(happiness, 100);
+        console.log("saveHappiness from storage:", cappedHappiness)
         await AsyncStorage.setItem(HAPPINESS_KEY, cappedHappiness.toString());
     } catch (error) {
         console.error('Error saving happiness value:', error);
@@ -16,6 +17,7 @@ export const saveHappiness = async (happiness) => {
 export const loadHappiness = async () => {
     try {
         const happiness = await AsyncStorage.getItem(HAPPINESS_KEY);
+        console.log("loadHappiness from storage:", happiness)
         return happiness !== null ? parseInt(happiness) : 0;
     } catch (error) {
         console.error('Error loading happiness value:', error);
